6bd4512a70988e7e707ccb759c30863f86f57d1a
Folkert <folkert@folkertdev.nl>
2019-08-23 22:37:43 +0200

    elm format

--- src/HTree.elm
+++ src/HTree.elm
@@ -1,8 +1,9 @@
-module HTree exposing (fromList, tagWithDepth, toList, toOutline, depth, nodeCount)
+module HTree exposing
+    ( fromList, toList, toOutline, depth, nodeCount
+    , tagWithDepth
+    )
 
-{-|
-
-Convert hierarchical lists to a rose tree,
+{-| Convert hierarchical lists to a rose tree,
 convert a rose tree to a list of node labels,
 convert a rose tree to a string representing the
 corresponding outline, etc. See HTreeExample.elm.
@@ -11,27 +12,27 @@ corresponding outline, etc. See HTreeExample.elm.
 
 -}
 
-
-import Tree exposing(Tree, singleton)
-import Tree.Zipper as Zipper exposing(Zipper)
-
+import Tree exposing (Tree, singleton)
+import Tree.Zipper as Zipper exposing (Zipper)
 
 
-{-|  Suppose give an list of items of type `a`, a "root" element
+{-| Suppose give an list of items of type `a`, a "root" element
 of type `a`, and a function that determines the level of an item.
-The function `fromList` returns  the corresponding rose tree.
+The function `fromList` returns the corresponding rose tree.
 For an example, consider the outline given by the string
-`Example.Test.o3:
+\`Example.Test.o3:
 
     A
-      p
+        p
         1
         2
         3
-      q
+        q
+
     B
-      r
-      s
+        r
+        s
+
     C
 
 Then we have
@@ -52,29 +53,37 @@ Then we have
 
 The function `HS.level` returns the number of leading spaces divided by 2 --
 integer division, no remainder.
+
 -}
-fromList : a -> (a -> Int) -> (List a) -> Tree a
-fromList rooLabel  level lst =
+fromList : a -> (a -> Int) -> List a -> Tree a
+fromList rooLabel level lst =
     lst
-      |> List.foldl (\s z -> step level s z) (Zipper.fromTree (Tree.singleton rooLabel))
-      |> Zipper.toTree
+        |> List.foldl (\s z -> step level s z) (Zipper.fromTree (Tree.singleton rooLabel))
+        |> Zipper.toTree
 
 
-step : (a -> Int) -> a ->  Zipper a -> Zipper a
+step : (a -> Int) -> a -> Zipper a -> Zipper a
 step level s z =
     let
-        ld = levelDifference level s z
+        ld =
+            levelDifference level s z
     in
-      case ld of
-        Nothing ->  appendAtFocus level s z
-        Just 0 -> appendAtFocus level s z
-        Just 1 -> addChildAtFocus level s z
-        _ ->
-           let
-              levelsBack = (negate (ld |> Maybe.withDefault 0))
+    case ld of
+        Nothing ->
+            appendAtFocus level s z
 
-           in
-           addAtNthParent level levelsBack s z
+        Just 0 ->
+            appendAtFocus level s z
+
+        Just 1 ->
+            addChildAtFocus level s z
+
+        _ ->
+            let
+                levelsBack =
+                    negate (ld |> Maybe.withDefault 0)
+            in
+            addAtNthParent level levelsBack s z
 
 
 {-| The `tag` function transforms a tree of items into
@@ -88,16 +97,19 @@ depth of `a` in the tree.
        ,Tree ("C",1) []]
 
 -}
-tagWithDepth : Tree a -> Tree (a, Int)
+tagWithDepth : Tree a -> Tree ( a, Int )
 tagWithDepth t =
-    tagWithDepth_ (t, 0)
+    tagWithDepth_ ( t, 0 )
 
-tagWithDepth_ : (Tree a,  Int) -> Tree (a, Int)
-tagWithDepth_ (t, k) =
+
+tagWithDepth_ : ( Tree a, Int ) -> Tree ( a, Int )
+tagWithDepth_ ( t, k ) =
     let
-        c = Tree.children t
-     in
-        Tree.tree (Tree.label t, k) (List.map (\t_ -> tagWithDepth_ (t_, (k + 1))) c)
+        c =
+            Tree.children t
+    in
+    Tree.tree ( Tree.label t, k ) (List.map (\t_ -> tagWithDepth_ ( t_, k + 1 )) c)
+
 
 {-| Map a tree to a list of node contents:
 
@@ -115,7 +127,9 @@ tagWithDepth_ (t, k) =
 -}
 toList : Tree b -> List b
 toList t =
-     Tree.flatten t
+    Tree.flatten t
+
+
 
 -- ADDING THINGS --
 
@@ -123,56 +137,81 @@ toList t =
 appendAtFocus : (a -> Int) -> a -> Zipper a -> Zipper a
 appendAtFocus level s z =
     let
-        t = Zipper.tree z
-        newTree = Tree.appendChild (singleton s) t
+        t =
+            Zipper.tree z
+
+        newTree =
+            Tree.appendChild (singleton s) t
     in
-      Zipper.replaceTree newTree z
+    Zipper.replaceTree newTree z
 
 
 addChildAtFocus : (a -> Int) -> a -> Zipper a -> Zipper a
 addChildAtFocus level s z =
     case Zipper.lastChild z of
-        Nothing -> z
-        Just zz -> appendAtFocus level s zz
+        Nothing ->
+            z
+
+        Just zz ->
+            appendAtFocus level s zz
+
 
 addChildAtParentOfFocus : (a -> Int) -> a -> Zipper a -> Zipper a
 addChildAtParentOfFocus level s z =
     case Zipper.parent z of
-        Nothing -> z
-        Just zz -> appendAtFocus level s zz
+        Nothing ->
+            z
+
+        Just zz ->
+            appendAtFocus level s zz
+
 
 addAtNthParent : (a -> Int) -> Int -> a -> Zipper a -> Zipper a
 addAtNthParent level k s z =
     case manyParent k z of
-        Nothing -> z
-        Just zz -> appendAtFocus level s zz
+        Nothing ->
+            z
+
+        Just zz ->
+            appendAtFocus level s zz
+
 
 nthParentOfFocus : Int -> Zipper a -> Zipper a
 nthParentOfFocus k z =
     case manyParent k z of
-        Nothing -> z
-        Just zz -> zz
+        Nothing ->
+            z
+
+        Just zz ->
+            zz
+
+
 
 -- MOVING AROUND --
 
+
 manyParent : Int -> Zipper a -> Maybe (Zipper a)
 manyParent k z =
-     let
-         zz = Zipper.parent z
-      in
-        iterate (k-1) (\zi -> Maybe.andThen Zipper.parent zi) zz
+    let
+        zz =
+            Zipper.parent z
+    in
+    iterate (k - 1) (\zi -> Maybe.andThen Zipper.parent zi) zz
 
 
 manyBackward : Int -> Zipper a -> Maybe (Zipper a)
 manyBackward k z =
-   if k < 0 then
-     Nothing
-   else if k == 0 then
-     Just z
-   else
-     let
-         zz = Zipper.backward z
-      in
+    if k < 0 then
+        Nothing
+
+    else if k == 0 then
+        Just z
+
+    else
+        let
+            zz =
+                Zipper.backward z
+        in
         iterate k (\zi -> Maybe.andThen Zipper.backward zi) zz
 
 
@@ -188,15 +227,14 @@ iterate k f x =
 levelOfLastChild : (a -> Int) -> Zipper a -> Maybe Int
 levelOfLastChild level z =
     Zipper.lastChild z
-      |> Maybe.map Zipper.tree
-      |> Maybe.map Tree.label
-      |> Maybe.map level
+        |> Maybe.map Zipper.tree
+        |> Maybe.map Tree.label
+        |> Maybe.map level
 
 
 levelDifference : (a -> Int) -> a -> Zipper a -> Maybe Int
 levelDifference level s z =
-     Maybe.map2 (-) (Just <| level s) (levelOfLastChild level z)
-
+    Maybe.map2 (-) (Just <| level s) (levelOfLastChild level z)
 
 
 
@@ -207,16 +245,19 @@ levelDifference level s z =
 
     > depth t
     2 : I
+
 -}
 depth : Tree a -> Int
 depth t =
     let
-       c = Tree.children t
+        c =
+            Tree.children t
     in
     if c == [] then
-      0
+        0
+
     else
-     1 + listMax (List.map depth c)
+        1 + listMax (List.map depth c)
 
 
 {-| nodecount t is the number of notes in the tree t
@@ -225,16 +266,19 @@ depth t =
     8 : Int
 
 The count includes the root.
+
 -}
 nodeCount : Tree a -> Int
 nodeCount t =
     let
-           c = Tree.children t
-     in
-     if c == [] then
-           1
-         else
-          1 + List.sum (List.map nodeCount c)
+        c =
+            Tree.children t
+    in
+    if c == [] then
+        1
+
+    else
+        1 + List.sum (List.map nodeCount c)
 
 
 listMax : List Int -> Int
@@ -242,7 +286,9 @@ listMax ints =
     List.foldl (\i acc -> max i acc) 0 ints
 
 
+
 -- STRING REPRESENTATION --
+
 
 {-| Given a function that maps labels to strings, construct
 a string that represents the tree as an outline.
@@ -260,25 +306,38 @@ The string returned is
             r
             s
         C
+
 -}
 toOutline : (a -> String) -> Tree a -> String
 toOutline stringOfLabel t =
-   t
-     |> tagWithDepth
-     |> toOutline_ stringOfLabel
+    t
+        |> tagWithDepth
+        |> toOutline_ stringOfLabel
 
 
-toOutline_ : (a -> String) -> Tree (a, Int) -> String
+toOutline_ : (a -> String) -> Tree ( a, Int ) -> String
 toOutline_ stringOfLabel t =
     let
-        stringOfLabel_ t_ = Tree.label t_ |> Tuple.first |> stringOfLabel
-        c = Tree.children t
-        (label_, level_) = Tree.label t |> (\(la, le) -> ( stringOfLabel_ t, le))
-        n  = 2 * (level_ - 1 )
-        prefix = String.repeat n " "
-        lab =  prefix ++ label_
+        stringOfLabel_ t_ =
+            Tree.label t_ |> Tuple.first |> stringOfLabel
+
+        c =
+            Tree.children t
+
+        ( label_, level_ ) =
+            Tree.label t |> (\( la, le ) -> ( stringOfLabel_ t, le ))
+
+        n =
+            2 * (level_ - 1)
+
+        prefix =
+            String.repeat n " "
+
+        lab =
+            prefix ++ label_
     in
-      if c == [] then
-       lab
-      else
+    if c == [] then
+        lab
+
+    else
         lab ++ "\n" ++ (List.map (toOutline_ stringOfLabel) c |> String.join "\n")
